ab7ebc201adfa85b0f14739a7513a24422bfebff
Teknasyon-S <teknasyon-s@teknasyon-s-macbook-air.local>
2018-11-28 13:42:42 +0300

    Initial Commit

---
+++ InstagramClone/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  InstagramClone
+//
+//  Created by Teknasyon-S on 28.11.2018.
+//  Copyright © 2018 Hazal Eroglu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
